05a6bba370c3a0cd7962b75e1264a82f8d0bfbe6
Equinox-13 <quaid.johar33@gmail.com>
2020-01-15 10:05:57 +0530

    Added template and passed dictionary to it through render

--- wordcount_project/views.py
+++ wordcount_project/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+# This is a wrong approach
+# def home(request):
+# 	return HttpResponse("hello")
+
+
+# Using render to call template and pass a dictionary
 def home(request):
-	return HttpResponse("hello")
+	return render(request, 'home.html', {'hithere':'this is me'})
