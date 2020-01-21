9a8eed3bf5637ef77b706d2dd0c934372bea0d43
Equinox-13 <quaid.johar33@gmail.com>
2020-01-21 10:08:31 +0530

    Added 'start again' url in count page

--- wordcount_project/urls.py
+++ wordcount_project/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('count/', views.count, name='count')
 ]
